a9701d4aa9c3f1971da158531141446a8539f7a5
Chinmayi <ambatichinmayi@gmail.com>
2019-09-27 13:49:08 -0500

    Done!

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const add = (x, y) => { return x + y }
+const mul = (x, y, z) => { return x *y *z }
 
 const validate = async (event) => {
   console.log(`triggered validate on ${event.target.id}`)
@@ -13,31 +13,26 @@ const validate = async (event) => {
 
 const updateWithAdd = async (event) => {
   document.querySelector('#result').innerHTML = ''
-  if (document.querySelector('#depth').checkValidity() && document.querySelector('#width').checkValidity() && document.querySelector('#height').checkValidity()) {
+  if (document.querySelector('#depth').checkValidity() && document.querySelector('#width').checkValidity() && document.querySelector('#height').checkValidity() ) {
     const regex = /[^a-zA-Z_]/g
     const k = parseInt(document.querySelector('#depth').value)
     const i = parseInt(document.querySelector('#width').value)
     const j = parseInt(document.querySelector('#height').value)
-    const ans = `${i}, your voulme is ${mul(k, i, j)}.`
+    const ans = `Your volume is ${mul(i, j, k)}.`
+
     document.querySelector('#result').innerHTML = ans
   }
 }
 
-const updateWithJoke = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
-  const response = await fetch(url)
-  const obj = await response.json()
-  const joke = obj.value.joke || 'No joke for you.'
-  document.querySelector('#result').innerHTML = joke
-}
+
 
 // delegate to dynamic elements (e.g. when testing)
 // focusout is like blur, but it bubbles up
 
 document.addEventListener('focusout', event => {
   if ((event.target && event.target.id === 'depth') ||
-    (event.target && event.target.id === 'width') || (event.target && event.target.id === 'height') ) {
+    (event.target && event.target.id === 'width') ||
+    (event.target && event.target.id === 'height')){
     validate(event)
   }
 })
@@ -46,6 +41,3 @@ document.addEventListener('click', event => {
   if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
 })
 
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
-})
